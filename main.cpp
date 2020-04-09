be4540d29311a9ff25f87774f832b327b26caf3e
Bob(ZhdanovaED) <bob@example.com>
2020-04-09 16:34:41 +0300

    code: min

--- main.cpp
+++ main.cpp
@@ -13,13 +13,24 @@ int main()
     <<"A*B="<<a*b<<'\n'
     <<"A/B="<<a/b<<'\n';
 
+
     if (a>b)
     {
-        cout<<a;
+        cout<<"max="<<a;
     }
     else
     {
-        cout<<b;
+        cout<<"max="<<b;
     }
+
+  if (a<b)
+  {
+      cout<<"min="<<a;
+  }
+  else
+  {
+      cout<<"min="<<b;
+  }
+
     return 0;
 }
